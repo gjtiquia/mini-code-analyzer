ab43dfceabcd09665647f18ecbb4b44a06ec499e
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-23 17:01:19 +0800

    chore: setup test environment

---
+++ __tests__/integration.test.ts
@@ -0,0 +1,5 @@
+describe("Integration Test", () => {
+    it("should pass", () => {
+        expect(true).toBeTruthy();
+    })
+})
--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  modulePathIgnorePatterns: ["directory-to-test"]
 };
